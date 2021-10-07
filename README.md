c025518bd25e7a4941df7314ae52ebd3a4ab37af
Emanuele Macri <emanuele.macri@prima.it>
2021-10-07 11:55:17 +0200

    first webpack configuration

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-### Pyxis Scss
+# pyxis-scss
+
+> An implementation of Pyxis Design System in HTML and SCSS.
+
+<img alt="Pyxis Design System" src="https://primait.invisionapp.com/assets/A_MGFjZjlkZDY2YjhlM2JmOcFk5Abzk3TRKxwC2DizMEC_4iALnd1rcz3A4sLpUZhZLAeTWF6uAwL4P5Wms5DGkWJqJGcTJbrH-nJvJ-Ijn0aqwJqoMNrGTe-44N-QSOJmH0sBBqsqThhHMvL4qhtgVA==" width="100%" />
+
+
+## Build Pyxis on your machine
+
+`yarn build`
+
+## Lint Pyxis
+`yarn lint`
+
+To autofix your errors run
+`yarn lint:autofix`
+
+## Start dev server
+`yarn serve`
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+// Webpack uses this to work with directories
+const path = require('path');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const stylelintOptions = {
+  configFile: '.stylelintrc',
+  context: 'scss',
+  emitError: true,
+  emitWarning: true,
+  failOnWarning: true,
+  ignoreDisables: true,
+  syntax: 'scss',
+}
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    pyxis: './pyxis.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].bundle.js'
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        include: path.resolve(__dirname, 'src', 'scss'),
+        use: [
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|svg|png|gif|ico|eot|ttf|woff2?)(\?v=\d+\.\d+\.\d+)?$/i,
+        type: 'asset/resource',
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new StylelintPlugin(stylelintOptions),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
